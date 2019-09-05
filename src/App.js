a0f5b0c33b04a356e44af837113c3e065df39b1b
Jamie Wright <jamie@brilliantfantastic.com>
2019-09-05 09:06:44 -0500

    Moved App.js to src

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
-import HomeScreen from "./src/screens/HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 const Navigator = createSwitchNavigator(
   {
